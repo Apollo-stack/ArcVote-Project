c63b33824bc8f92c6946656af89d064f5b1c93d9
Apollo-stack <Matheusramon0208@gmail.com>
2025-12-04 14:33:47 -0300

    inclusao de mais opções de escolha

--- ArcVote.sol
+++ ArcVote.sol
@@ -1,39 +1,41 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.19; // Versão segura para evitar erros de Gas
+pragma solidity 0.8.19;
 
 contract ArcVote {
 
     struct Candidato {
-        string nome;  
+        string nome;
         uint256 contaVotos; 
     }
 
     Candidato[] public candidatos; 
     
-    mapping (address => bool) public jaVotou; 
-
-    // Evento para avisar o site
+    mapping (address => bool) public jaVotou;
+    
     event votoComputado(address eleitor, string nomeCandidato);
 
     constructor (){
-        // Criando os candidatos iniciais
+        // Pode adicionar ou remover quantos quiser aqui agora!
         candidatos.push(Candidato("ArcToken", 0));
         candidatos.push(Candidato("Matic", 0));
         candidatos.push(Candidato("PolkaDot", 0));
         candidatos.push(Candidato("USDC", 0));
+        candidatos.push(Candidato("Bitcoin", 0)); // Exemplo novo
     }
 
     function votar (uint256 _candidatoIndex) public {
-        
-        require(!jaVotou[msg.sender], "Apenas um voto por carteira é aceito!"); 
-
+        require(!jaVotou[msg.sender], "Apenas um voto por carteira!");
         require(_candidatoIndex < candidatos.length, "Token invalido.");
 
-        // Atualizando os dados
         jaVotou[msg.sender] = true;
         candidatos[_candidatoIndex].contaVotos += 1; 
 
-        // Passando os dados reais para o evento
         emit votoComputado(msg.sender, candidatos[_candidatoIndex].nome);
+    }
+
+    // --- NOVA FUNÇÃO ---
+    // Essa função permite o site descobrir o tamanho da lista (length)
+    function contarCandidatos() public view returns (uint256) {
+        return candidatos.length;
     }
 }
